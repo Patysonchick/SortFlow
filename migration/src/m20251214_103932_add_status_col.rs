437554ceadd49b2e2f0b2ef6500e18858f6469da
Эдгар Томский <edgarkozlov3229@gmail.com>
2025-12-14 20:24:52 +0300

    TODO! дописать логику для колонки status

---
+++ migration/src/m20251214_103932_add_status_col.rs
@@ -0,0 +1,24 @@
+use sea_orm_migration::{prelude::*, schema::*};
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .alter_table(
+                Table::alter()
+                    .table("bin")
+                    .add_column(unsigned("status").default(0))
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .alter_table(Table::alter().table("bin").drop_column("status").to_owned())
+            .await
+    }
+}
