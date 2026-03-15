0581f9b811fbc88c3d7f01b70dff749799e108fa
Эдгар Томский <edgarkozlov3229@gmail.com>
2026-03-15 19:20:53 +0300

    Added pin initing

--- sortflow-firmware/conveyor/src/main.cpp
+++ sortflow-firmware/conveyor/src/main.cpp
@@ -14,13 +14,34 @@ void TaskHardware(void *pvParameters);
 
 void setup() {
   Serial.begin(115220);
+  Serial2.begin(115200, SERIAL_8N1, PIN_UART2_RX, PIN_UART2_TX); // ESP32-CAM UART2
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  // L298N
+  pinMode(PIN_MOTOR_ENA, OUTPUT);
+  pinMode(PIN_MOTOR_IN1, OUTPUT);
+  pinMode(PIN_MOTOR_IN2, OUTPUT);
+
+  // SG90
+  pinMode(PIN_SERVO_1, OUTPUT);
+  pinMode(PIN_SERVO_2, OUTPUT);
+  pinMode(PIN_SERVO_3, OUTPUT);
+
+  // KY-032
+  // 34, 35 - ONLY INPUT!!!
+  pinMode(PIN_SENSOR_1, INPUT);
+  pinMode(PIN_SENSOR_2, INPUT);
 
   pinMode(LED_BUILTIN, OUTPUT);
 
   while(!Serial) {}
   Serial.println(F("Conveyor starting..."));
-  blink_n(LED_BUILTIN, 2, 100);
+
+  Serial.println(F("Setuping UART2..."));
+  while(!Serial2) {}
+  Serial2.println(F("UART2 started!"));
+
+  blink_n(LED_BUILTIN, 2, 100); // UART inited signal
   
   while(WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -28,7 +49,7 @@ void setup() {
     blink_n(LED_BUILTIN, 2, 50);
   }
   Serial.println(F("Connected to WiFi!"));
-  blink_n(LED_BUILTIN, 3, 250);
+  blink_n(LED_BUILTIN, 3, 250); // WiFi inited signal
 
   stateSemaphore = xSemaphoreCreateBinary();
 
@@ -51,6 +72,9 @@ void setup() {
   //   NULL,
   //   0
   // );
+
+  Serial.println(F("Init complete!"));
+  blink_n(LED_BUILTIN, 4, 250);
 }
 
 void loop() {
