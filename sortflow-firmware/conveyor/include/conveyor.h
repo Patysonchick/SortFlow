403c46351cd4d8b62b58c92aa3007ce7ada5f11d
Эдгар Томский <edgarkozlov3229@gmail.com>
2026-03-16 11:59:53 +0300

    Added conveyor funcs

---
+++ sortflow-firmware/conveyor/include/conveyor.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <Arduino.h>
+
+void conveyor_forward();
+void conveyor_backforward();
+void conveyor_stop();
+void conveyor_start();
---
+++ sortflow-firmware/conveyor/src/conveyor.cpp
@@ -0,0 +1,24 @@
+#include "conveyor.h"
+#include "config.h"
+
+void conveyor_forward() {
+    digitalWrite(PIN_MOTOR_ENA, LOW);
+    digitalWrite(PIN_MOTOR_IN1, HIGH);
+    digitalWrite(PIN_MOTOR_IN1, LOW);
+    digitalWrite(PIN_MOTOR_ENA, HIGH);
+}
+
+void conveyor_backforward() {
+    digitalWrite(PIN_MOTOR_ENA, LOW);
+    digitalWrite(PIN_MOTOR_IN1, LOW);
+    digitalWrite(PIN_MOTOR_IN1, HIGH);
+    digitalWrite(PIN_MOTOR_ENA, HIGH);
+}
+
+void conveyor_stop() {
+    digitalWrite(PIN_MOTOR_ENA, LOW);
+}
+
+void conveyor_start() {
+    digitalWrite(PIN_MOTOR_ENA, HIGH);
+}
